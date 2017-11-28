6c56e49e91ad63c48fdbfc449aecf8494d100d9e
Juan <juan.elca@gmail.com>
2017-11-28 19:42:51 -0300

    Update Servicio.java

--- android/java/com/example/lautaro/clasesoa/Servicio.java
+++ android/java/com/example/lautaro/clasesoa/Servicio.java
@@ -1,27 +1,42 @@
 package com.example.lautaro.clasesoa;
 
-import android.app.IntentService;
-import android.content.Intent;
-import android.support.annotation.Nullable;
-
 /**
- * Created by lautaro on 20/11/17.
+ * Created by juan on 28/11/17.
  */
 
-public class Servicio extends IntentService {
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.util.Log;
 
-        private SocketDevice s;
+public class Servicio extends Service{
 
-    public Servicio() {
-        super("AutomacService");
+    private SocketDevice s;
+
+
+    @Override
+    public IBinder onBind(Intent intent) {
+
+        return null;
     }
 
-        @Override
-        protected void onHandleIntent(@Nullable Intent intent) {
-
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
         s = SocketDevice.getInstance();
         String comandoArduino  = intent.getStringExtra("comando");
-        if(s.isAvailable())
+        if(s.isAvailable()) {
             s.enviar(comandoArduino);
+        }
+        return Service.START_STICKY;
     }
+
+
+    @Override
+    public void onDestroy() {
+        /*  */
+        super.onDestroy();
+        // Log.d(TAG, "FirstService destroyed");
+    }
+
 }
